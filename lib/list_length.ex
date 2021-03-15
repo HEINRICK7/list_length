ca40261617825600333a0e3698df09863a6df5b9
Henrique <Carlos Henrique>
2021-03-15 16:36:53 -0300

    using Enum Enum.flat_map and Enum.count

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,4 +1,13 @@
 defmodule ListLength do
-  @spec call(list) :: non_neg_integer
-  def call(list), do: length(list)
+  def filterInteger do
+
+    ["1", "3", "6", "43", "banana", "6", "abc"] |>
+    Enum.flat_map( fn string ->
+      case Integer.parse(string) do
+        {int, _rest} -> [int]
+        :error -> []
+      end
+    end) |>
+    Enum.count(fn x -> rem(x, 2) == 1 end)
+  end
 end
